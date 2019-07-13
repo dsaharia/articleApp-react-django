31377dc9393e75cf551f697ebaba402f6e646234
Dhiraj Saharia <dsaharia@outlook.com>
2019-07-13 22:14:09 +0530

    front end started upto nav bar

---
+++ frontend/client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import CustomLayout from './components/containers/Layout';
+
+function App() {
+  return (
+    <div className="App">
+      <CustomLayout>
+      
+      </CustomLayout>
+    </div>
+  );
+}
+
+export default App;
