56c61d4587ca994e0ed4b9234821f8beb5fd97de
Dhiraj Saharia <dsaharia@outlook.com>
2019-07-26 23:55:15 +0530

    new view sets used

--- backend/src/articles/urls.py
+++ backend/src/articles/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path, include
-from .views import ArticleListView, ArticleDetailView
+from articles.views import ArticleViewSet
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path('', ArticleListView.as_view()),
-    path('<pk>', ArticleDetailView.as_view()),
-]
+router = DefaultRouter()
+router.register(r'', ArticleViewSet, basename='articles')
+urlpatterns = router.urls
+
+
+# from .views import ArticleListView, ArticleDetailView, ArticleDetailView
+
+# urlpatterns = [
+#     path('', ArticleListView.as_view()),
+#     path('create/', ArticleDetailView.as_view()),
+#     path('<pk>', ArticleDetailView.as_view()),
+# ]
--- backend/src/articles/views.py
+++ backend/src/articles/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+# from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
+from rest_framework import viewsets
 from .serializers import ArticleSerializer
 from .models import Article
 
+class ArticleViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing user instances.
+    """
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
 # Create your views here.
 
-class ArticleListView(ListAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
+# class ArticleListView(ListAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
 
-class ArticleDetailView(RetrieveAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
+# class ArticleDetailView(RetrieveAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+
+# class ArticleCreateView(CreateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
 
 
